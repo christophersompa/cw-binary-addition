6064836006a78bc248cdee9bd1f93933e4b4bc0a
Christopher <cgtsompa@gmail.com>
2024-10-13 23:48:00 +0100

    refactor

--- binary-addition/Program.cs
+++ binary-addition/Program.cs
@@ -8,20 +8,14 @@ namespace binary_addition
         {
             /*
              * Implement a function that adds two number together and returns their sum in binary
-             * This conversion can be done before, or after the addition.
-             * 
-             * The binary number return should be a string
+             * This conversion can be done before, or after the addition. The binary number return should be a string
              */
 
             Console.WriteLine(AddBinary(1,1));
             Console.ReadLine();
         }
 
-        public static string AddBinary(int a , int b )
-        {
-            int sum = a + b;
-
-            return Convert.ToString(sum , 2);
-        }
+        public static string AddBinary(int a , int b ) => Convert.ToString(a + b , 2);
+        
     }
 }
