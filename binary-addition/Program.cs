5f30b4d66eae9bb52eaddb5fc9be9730e7bf8dbc
Christopher <cgtsompa@gmail.com>
2024-10-13 23:46:13 +0100

    Add project files.

---
+++ binary-addition/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace binary_addition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Implement a function that adds two number together and returns their sum in binary
+             * This conversion can be done before, or after the addition.
+             * 
+             * The binary number return should be a string
+             */
+
+            Console.WriteLine(AddBinary(1,1));
+            Console.ReadLine();
+        }
+
+        public static string AddBinary(int a , int b )
+        {
+            int sum = a + b;
+
+            return Convert.ToString(sum , 2);
+        }
+    }
+}
